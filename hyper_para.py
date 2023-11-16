35296e1d8b26a314ef2dbfe701b209caee843392
Joseph <zhangdk99@gmail.com>
2023-11-16 21:15:47 +0800

    first commit

---
+++ hyper_para.py
@@ -0,0 +1,3 @@
+DATA_PATH = 'D:/data/luna16/'
+CACHE_PATH = 'D:/.cache/'
+EPOCHS = 20
---
+++ utils.py
@@ -0,0 +1,103 @@
+import torch
+import matplotlib.pyplot as plt
+
+
+def box_corner_to_center(boxes: torch.tensor):
+    x1, y1, x2, y2 = boxes[:, 0], boxes[:, 1], boxes[:, 2], boxes[:, 3]
+    cx = (x1 + x2) / 2
+    cy = (y1 + y2) / 2
+    w = x2 - x1
+    h = y2 - y1
+    return torch.stack((cx, cy, w, h), dim=-1)
+
+
+def box_center_to_corner(boxes: torch.tensor):
+    cx, cy, w, h = boxes[:, 0], boxes[:, 1], boxes[:, 2], boxes[:, 3]
+    x1 = cx - 0.5 * w
+    y1 = cy - 0.5 * h
+    x2 = cx + 0.5 * w
+    y2 = cy + 0.5 * h
+    return torch.stack((x1, y1, x2, y2), dim=-1)
+
+
+def bbox_to_rect(bbox: torch.tensor, color: str):
+    return plt.Rectangle(xy=(bbox[0], bbox[1]),
+                         width=bbox[2] - bbox[0], height=bbox[3] - bbox[1],
+                         fill=False, edgecolor=color, linewidth=2)
+
+
+def multi_box_prior(data, sizes, ratios):
+    """
+    以每个像素为中心生成具有不同形状的锚框
+
+    data: 输入图片，（batch_size, channel, height, width）
+
+    sizes: scale缩放比
+
+    ratios: 宽高比
+    """
+    in_height, in_width = data.shape[-2:]
+    device, num_sizes, num_ratios = data.device, len(sizes), len(ratios)
+    boxes_per_pixel = (num_sizes + num_ratios - 1)
+    size_tensor = torch.tensor(sizes, device=device)
+    ratio_tensor = torch.tensor(ratios, device=device)
+
+    # 为了将锚框中心点移动到像素的中心，需要设置偏移量
+    # 因为1像素的高宽均为1，我们选择偏移量中心为0.5
+    offset_h, offset_w = 0.5, 0.5
+    steps_h, steps_w = 1.0 / in_height, 1.0 / in_width  # 在x轴，y轴上缩放步长
+
+    # 生成锚框的所有中心点
+    center_h = (torch.arange(in_height, device=device) + offset_h) * steps_h
+    center_w = (torch.arange(in_width, device=device) + offset_w) * steps_w
+    shift_y, shift_x = torch.meshgrid(center_h, center_w, indexing='ij')
+    shift_y, shift_x = shift_y.reshape(-1), shift_x.reshape(-1)
+
+    w = torch.cat((size_tensor * torch.sqrt(ratio_tensor[0]),
+                   sizes[0] * torch.sqrt(ratio_tensor[1:]))) \
+        * in_height / in_width
+    h = torch.cat((size_tensor / torch.sqrt(ratio_tensor[0]),
+                   sizes[0] * torch.sqrt(ratio_tensor[1:])))
+    anchor_manipulations = torch.stack((-w, -h, w, h)).T.repeat(
+        in_height * in_width, 1) / 2
+
+    out_grid = torch.stack([shift_x, shift_y, shift_x, shift_y],
+                           dim=1).repeat_interleave(boxes_per_pixel, dim=0)
+    output = out_grid + anchor_manipulations
+    return output.unsqueeze(0)
+
+
+def show_bboxes(axes, bboxes, labels=None, colors=None):
+    """显示所有边界框"""
+
+    def _make_list(obj, default_values=None):
+        if obj is None:
+            obj = default_values
+        elif not isinstance(obj, (list, tuple)):
+            obj = [obj]
+        return obj
+
+    labels = _make_list(labels)
+    colors = _make_list(colors, ['b', 'g', 'r', 'm', 'c'])
+    for i, bbox in enumerate(bboxes):
+        color = colors[i % len(colors)]
+        rect = bbox_to_rect(bbox.detach().numpy(), color)
+        axes.add_patch(rect)
+        if labels and len(labels) > i:
+            text_color = 'k' if color == 'w' else 'w'
+            axes.text(rect.xy[0], rect.xy[1], labels[i],
+                      va='center', ha='center', fontsize=9, color=text_color,
+                      bbox=dict(facecolor=color, lw=0))
+
+
+def box_iou(boxes1, boxes2):
+    box_area = lambda boxes: ((boxes[:, 2] - boxes[:, 0]) *
+                              (boxes[:, 3] - boxes[:, 1]))
+    areas1 = box_area(boxes1)
+    areas2 = box_area(boxes2)
+    inter_upperlefts = torch.max(boxes1[:, None, :2], boxes2[:, :2])
+    inter_lowerrights = torch.min(boxes1[:, None, 2:], boxes2[:, 2:])
+    inters = (inter_lowerrights - inter_upperlefts).clamp(min=0)
+    inter_areas = inters[:, :, 0] * inters[:, :, 1]
+    union_areas = areas1[:, None] + areas2 - inter_areas
+    return inter_areas / union_areas
